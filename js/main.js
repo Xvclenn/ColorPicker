17771489f26c7d179f99dbbd56f2c42102947dd2
Xvclenn <https://github.com/Xvclenn>
2023-01-03 17:05:24 +0800

    ColorPicker

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const b1 = document.getElementsByTagName("button")
+
+// console.log(b1)
+
+// b1[0].addEventListener("click", colorPicker1())
+
+function colorPicker1()
+{
+    document.getElementById("ti1").src = "/img/brown.png"
+}
+
+// b1[1].addEventListener("click", colorPicker2())
+
+function colorPicker2()
+{
+    document.getElementById("ti1").src = "/img/purple.png"
+}
+
+// b1[2].addEventListener("click", colorPicker3())
+
+function colorPicker3()
+{
+    document.getElementById("ti1").src = "/img/green.png"
+}
